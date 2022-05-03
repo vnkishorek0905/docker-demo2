1caf0311a601c11f7db91846c665aa8c03a509a7
RARIGOVARD <RARIGOVARD@host.docker.internal>
2022-05-02 22:20:30 -0400

    added MyController and removed test class

---
+++ docker-demo2/src/main/java/com/kishore/playjava/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.kishore.playjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "Welcome to first docker demo project live session";
+	}
+}

39e0ad696f3f5e4b8d817599256c419f3a43f18c
RARIGOVARD <RARIGOVARD@host.docker.internal>
2022-05-02 22:08:48 -0400

    first commit

---
+++ docker-demo2/src/main/java/com/kishore/playjava/DockerDemo2Application.java
@@ -0,0 +1,13 @@
+package com.kishore.playjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemo2Application.class, args);
+	}
+
+}
